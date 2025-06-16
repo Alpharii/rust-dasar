9235a79c94320b19e96d693c8639c2a991ad3ff4
alpharii <alphari404@gmail.com>
2025-06-16 15:56:26 +0700

    variable & shadowing

--- belajar_rust_dasar/src/main.rs
+++ belajar_rust_dasar/src/main.rs
@@ -4,7 +4,35 @@ fn main() {
     println!("hello rust");
 }
 
+//Hello world
 #[test]
 fn hello_test(){
     println!("hello test");
+}
+
+//immutable variable
+#[test]
+fn variable_test(){
+    let name: &'static str = "Muhammad Bintang Alphari";
+    println!("Hello {}", name);
+}
+
+//mutable variable
+#[test]
+fn test_mutable(){
+    let mut name = "Muhammad Bintang Alphari";
+    println!("Hello {}", name);
+    
+    name = "Alphari";
+    println!("Hello {}", name);
+}
+
+//shadowing
+#[test]
+fn test_shadowing(){
+    let name: &'static str = "Muhammad Bintang Alphari";
+    println!("Hello {}", name);
+
+    let name: &'static str = "Alphari";
+    println!("Hello {}", name);
 }
